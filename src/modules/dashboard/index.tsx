229ab7851b960c55293496144b73e03b193c094e
aaryaag03@gmail.com <root@Aarya-Laptop.localdomain>
2023-06-13 13:44:36 +0530

    create dashboard highlight component

--- src/modules/dashboard/index.tsx
+++ src/modules/dashboard/index.tsx
@@ -1,13 +1,37 @@
 import { useState } from 'react'
-import { Stack,Button, ButtonGroup, Heading, Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+import { VStack,Text,Flex, Spacer,Box,Grid, GridItem,Stack,Button, ButtonGroup, Heading, Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
 import QuizCard from "./quizCard"
+import Highlight from "./highlight"
 const Dashboard = () => {
     return (
         <>
-        <Stack direction="row">
-        <QuizCard></QuizCard>
-        <QuizCard></QuizCard>
-        <QuizCard></QuizCard>
+        <Stack spacing={8}>
+            <Box>Quizio</Box>
+
+            <Highlight/>
+{/* 
+            <Box width={}  bg="#EBE7F2" display="flex" alignItems="center" justifyContent="center"> 
+                <Text color='brand'>
+                    Hehe
+                </Text>
+            </Box> */}
+
+
+
+            {/* <Flex wrap="wrap">
+                <QuizCard ></QuizCard>
+                <Spacer/>
+                <QuizCard></QuizCard>
+                <Spacer/>
+                <QuizCard></QuizCard>
+                <Spacer/>
+                <QuizCard></QuizCard>
+                <Spacer/>
+                <QuizCard></QuizCard>
+            </Flex> */}
+
+
+
         </Stack>
         {/* <Card>
             <CardHeader>
