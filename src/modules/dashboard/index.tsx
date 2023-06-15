04bb647f5391a6ccf6dc0fd378546326435a9b81
aarya <aaryaag03@gmail.com>
2023-06-15 14:59:11 +0530

    make highlight of the dashboard

--- src/modules/dashboard/index.tsx
+++ src/modules/dashboard/index.tsx
@@ -1,24 +1,94 @@
 import { useState } from 'react'
-import { VStack,Text,Flex, Spacer,Box,Grid, GridItem,Stack,Button, ButtonGroup, Heading, Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
-import QuizCard from "./quizCard"
-import Highlight from "./highlight"
+import {
+  VStack,
+  Text,
+  Flex,
+  Spacer,
+  Box,
+  Grid,
+  GridItem,
+  Stack,
+  Button,
+  ButtonGroup,
+  Heading,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+} from '@chakra-ui/react'
+import QuizCard from './quizCard'
+import Highlight from './highlight'
+import ScrollView from './scrollView'
+import TopNav from '../topNav'
+
 const Dashboard = () => {
-    return (
-        <>
-        <Stack spacing={8}>
-            <Box>Quizio</Box>
+  return (
+    <>
+      <Stack spacing={0}>
+        <TopNav />
 
-            <Highlight/>
-{/* 
-            <Box width={}  bg="#EBE7F2" display="flex" alignItems="center" justifyContent="center"> 
-                <Text color='brand'>
-                    Hehe
-                </Text>
-            </Box> */}
+        <Box width='100%' display='flex' justifyContent='center' alignItems='center'>
+          <Highlight />
+        </Box>
 
+        <Box width='100%' display='flex' justifyContent='center' alignItems='center'>
+          <Box mb={4} width='78%' display='flex'>
+            <Text
+              color='brand'
+              borderBottom='2px'
+              borderBottomColor='brand'
+              py={2}
+            >
+              Quizzes
+            </Text>
+            <Text borderBottom='2px' borderBottomColor='#939393' px={5} py={2}>
+              Created Quizzes
+            </Text>
+          </Box>
+        </Box>
 
+        <Box width='100%' display='flex' justifyContent='center' alignItems='center'>
+          <Box
+            width='78%'
+            height={12}
+            bg='rgba(235, 231, 242, 0.4)'
+            display='flex'
+            alignItems='center'
+            justifyContent='center'
+          >
+            <Heading opacity='2.5' fontSize='large' color='brand'>
+              Ongoing Quizzes
+            </Heading>
+          </Box>
+        </Box>
 
-            {/* <Flex wrap="wrap">
+        <Box width='100%' display='flex' justifyContent='center' alignItems='center'>
+          <ScrollView />
+        </Box>
+
+        <Box width='100%' display='flex' justifyContent='center' alignItems='center'>
+          <Box
+            width='78%'
+            height={12}
+            mt={4}
+            bg='rgba(235, 231, 242, 0.4)'
+            display='flex'
+            alignItems='center'
+            justifyContent='center'
+          >
+            <Heading opacity='2.5' fontSize='large' color='brand'>
+              Upcoming Quizzes
+            </Heading>
+          </Box>
+        </Box>
+
+        <Box width='100%' display='flex' justifyContent='center' alignItems='center'>
+          <ScrollView />
+        </Box>
+
+        
+
+        {/* <Flex wrap="wrap">
                 <QuizCard ></QuizCard>
                 <Spacer/>
                 <QuizCard></QuizCard>
@@ -29,17 +99,14 @@ const Dashboard = () => {
                 <Spacer/>
                 <QuizCard></QuizCard>
             </Flex> */}
-
-
-
-        </Stack>
-        {/* <Card>
+      </Stack>
+      {/* <Card>
             <CardHeader>
     <Heading size='md'></Heading>
   </CardHeader>
         </Card>
         <Button>Hello</Button> */}
-        </>
-    )
+    </>
+  )
 }
 export default Dashboard
