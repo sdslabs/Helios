e60dd0675bc4d9f42643fea47741cccd45475103
aarya <aaryaag03@gmail.com>
2023-06-18 01:51:36 +0530

    create profile page module

---
+++ src/modules/profile/index.tsx
@@ -0,0 +1,9 @@
+import { Text,Stack,Box,Card, CardHeader, CardBody, CardFooter, Button, Heading } from '@chakra-ui/react'
+
+const Profile=()=>{
+    return(
+        <Box>testing</Box>
+    )
+}
+
+export default Profile
