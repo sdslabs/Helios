a1b9e94ee682e5aa8767ac12c0a6ca56e1e933cb
Tanmay Bajaj <tanmaybajaj567@gmail.com>
2023-08-28 07:23:34 +0530

    fix: import resolves and package-lock

---
+++ craco.config.ts
@@ -0,0 +1,13 @@
+import path from 'path';
+module.exports = {
+  webpack: {
+    alias: {
+      '@common': path.resolve(__dirname, 'src/common'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@auth': path.resolve(__dirname, 'src/modules/auth'),
+      '@createQuiz': path.resolve(__dirname, 'src/modules/createQuiz'),
+      '@checkQuiz': path.resolve(__dirname, 'src/modules/checkQuiz'),
+      '@giveQuiz': path.resolve(__dirname, 'src/modules/giveQuiz'),
+    },
+  },
+};
