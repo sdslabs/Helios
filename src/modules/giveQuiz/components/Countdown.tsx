3435ad57bc58f1017b937e7372bb4806bcc18a1c
Altoiids <somyachawla0250@gmail.com>
2023-11-08 01:38:04 +0530

    change timer implementation

--- src/modules/giveQuiz/components/Countdown.tsx
+++ src/modules/giveQuiz/components/Countdown.tsx
@@ -4,21 +4,19 @@ import { Flex } from '@chakra-ui/react'
 import * as io from "socket.io-client";
 
 
-
 function Countdown() {
+  const [countdown, setCountdown] = useState(0);
   const [countHours, setCountHours] = useState('00')
   const [countMinutes, setCountMinutes] = useState('00')
   const [countSeconds, setCountSeconds] = useState('00')
 
-  useEffect(() => {
-    const socket = io.connect("http://localhost:3001");
-
-    const emitJoinQuizEvent = () => {
-      socket.emit('join_quiz', { quizId: "64f03422df4af65f96380c43", userId: "64f03422df4af65f96380c3e" });
-    };
+ 
+    const socket = io.connect("http://localhost:3000");
+    socket.emit('join_quiz', { quizId: "64f03422df4af65f96380c43", userId: "64f03422df4af65f96380c3e" });
 
     // Handle timer updates from the server
     socket.on('sendTime', (timeLeft) => {
+      setCountdown(timeLeft);
       // Convert 'timeLeft' to hours, minutes, and seconds and update the state
       const hours = Math.floor((timeLeft / 3600000) % 24);
       const minutes = Math.floor((timeLeft / 60000) % 60);
@@ -27,13 +25,33 @@ function Countdown() {
       setCountHours(hours.toString().padStart(2, '0'));
       setCountMinutes(minutes.toString().padStart(2, '0'));
       setCountSeconds(seconds.toString().padStart(2, '0'));
-      emitJoinQuizEvent();
+      
     });
-    emitJoinQuizEvent();
 
-  }, []);
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setCountdown((prevCountdown) => {
+         
+          if (prevCountdown > 0) {
+            return prevCountdown - 1;
+          }
+          return 0;
+        });
+      }, 1000);
 
+      
+      const hours = Math.floor((countdown / 3600) % 24);
+      const minutes = Math.floor((countdown / 60) % 60);
+      const seconds = Math.floor(countdown % 60);
+  
+      setCountHours(hours.toString().padStart(2, '0'));
+      setCountMinutes(minutes.toString().padStart(2, '0'));
+      setCountSeconds(seconds.toString().padStart(2, '0'));
+  
+      return () => clearInterval(interval);
+    }, [countdown]);
 
+   
   return (
     <Flex
       bgColor='v1'
