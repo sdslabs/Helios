20bd47ef8b2e11a7b990f34d1c3d9632e13995fc
Arpan Mahanty <arpanmahanty01@gmail.com>
2024-01-12 17:16:01 +0530

    removes popup based auth

---
+++ src/modules/auth/views/googleCallback.tsx
@@ -0,0 +1,40 @@
+import axiosInstance from '@auth/api/axiosInstance'
+import { Spinner } from '@chakra-ui/react'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+const querytoObject = (query: string) => {
+  const parameters = new URLSearchParams(query)
+  return Object.fromEntries(parameters.entries())
+}
+
+const GoogleCallback = (props: any) => {
+  const navigate = useNavigate()
+  const { Component = <div
+    style={{
+      width: '100vw',
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <Spinner size='xl' />
+  </div> } = props
+  useEffect(() => {
+    (async function getToken() {
+      const payload = querytoObject(window.location.search.split('?')[1])
+      const { code } = payload;
+      const data = await axiosInstance.post(`/auth/google/token?`, { code })
+      if (!(data.status === 200)) {
+        alert("Login failed")
+        navigate("/")
+      } else {
+        navigate("/")
+        window.location.reload()
+      }
+    })()
+  }, [])
+  return Component
+}
+
+export default GoogleCallback
