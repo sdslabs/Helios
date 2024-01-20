d688ddcd122c9dd02bd48bfeb66ec056d2ef148f
Lakshya Shishir <checkinglakshya@gmail.com>
2024-01-20 21:09:09 +0530

    sidenav rename

--- src/modules/checkQuiz/components/giveQuiz/CheckQuestionView.tsx
+++ src/modules/checkQuiz/components/giveQuiz/CheckQuestionView.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import WithSidebarWrapper from '@common/views/WithSidebarWrapper'
 import TopNav from '@common/components/TopNav'
-import SideNavContent from '@checkQuiz/components/giveQuiz/sideNav'
+import SideNavContent from '@checkQuiz/components/giveQuiz/SideNav'
 import SectionTopBar from '@checkQuiz/components/giveQuiz/SectionTopBar'
 import QuestionView from '@checkQuiz/components/giveQuiz/QuestionView'
 import { useNavigate, useParams } from 'react-router-dom'
