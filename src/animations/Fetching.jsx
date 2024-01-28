c656be9b501aea6b875811187e41e9659b67c74d
Tanmay Bajaj <tanmaybajaj567@gmail.com>
2024-01-28 22:19:05 +0530

    feat: fetching animation

---
+++ src/animations/Fetching.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Lottie from 'lottie-react';
+import fetchingAnimation from './fetching.json';
+
+const Fetching = () => (
+    <div className="fetching">
+        <Lottie animationData={fetchingAnimation} loop autoplay style={{ backgroundColor: '#FFFFFF' }} />
+    </div>
+);
+
+export default Fetching;
